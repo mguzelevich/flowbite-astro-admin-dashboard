7007f7286f9441e8911dc0cfa86e1cae6fd98865
Michael Guzelevich <mgu@mikh.pro>
2025-09-09 18:01:41 +0200

    change all urls to relative

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -24,10 +24,10 @@ export async function fetchData<Selected extends Endpoint>(endpoint: Selected) {
 		});
 }
 
-// NOTE: These helpers are useful for unifying paths, app-wide
-export function url(path = '') {
-	return `${import.meta.env.SITE}${import.meta.env.BASE_URL}${path}`;
-}
+// // NOTE: These helpers are useful for unifying paths, app-wide
+// export function url(path = '') {
+// 	return `${import.meta.env.SITE}${import.meta.env.BASE_URL}${path}`;
+// }
 
 // TODO: Remove old local assets from git history (to make cloning snappier).
 export function asset(path: string) {
